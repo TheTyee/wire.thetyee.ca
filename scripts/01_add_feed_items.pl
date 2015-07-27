d54bcce269953cc65353875a8711808f412ee516
Phillip Smith <ps@phillipadsmith.com>
2015-07-26 23:28:42 -0700

    Fix feedproxy, switch to Alchemy API, etc.
    + Fix UTF8 encoding issue with XML::FeedPP
    + Fix feedproxy URLs on items
    + Only find items from last 7 days
    + Remove HTML::Conent::Extractor
    + Switch to Alchemy API for text extraction
    + Decode entities in RSS/Atom titles
    + Change 'Not active' to 'stale' on Feed status

--- scripts/01_add_feed_items.pl
+++ scripts/01_add_feed_items.pl
@@ -15,6 +15,7 @@ use Data::Dumper;
 use DateTime;
 use HTTP::Date;
 use HTML::Content::Extractor;
+use HTML::Entities;
 use Lingua::EN::Summarize;
 use Mojo::UserAgent;
 use Try::Tiny;
@@ -26,6 +27,8 @@ die "You forgot the mode" unless $mode;
 
 my $conf   = Config::JFDI->new( name => "app.$mode", path => "$Bin/../" );
 my $config = $conf->get;
+my $ALCHEMY_API = 'http://access.alchemyapi.com/calls/url/URLGetCombinedData?';
+my $ALCHEMY_QUERY = '&extract=page-image,entity,keyword,taxonomy&showSourceText=1&quotations=1&outputMode=json&url=';
 my $dtformat = 'DateTime::Format::HTTP';
 my $dbh   = _dbh();
 my @sources = $dbh->resultset( 'Source' )->search()->all();
@@ -36,75 +39,75 @@ for my $source ( @sources ) {
     next unless $source->feed_url;
     say $source->feed_url;
     my $feed_obj = try {
-        XML::FeedPP->new( $source->feed_url );
+        XML::FeedPP->new( $source->feed_url, utf8_flag => 1 );
     } catch {
         say "Couldn't load the feed... skipping...";
         $source->update({ status => 'Could not load feed' });
         return;
-        next;
+        #next;
     };
-    my $status; 
     next unless $feed_obj;
+    my $ua = Mojo::UserAgent->new;
+    my $status; 
     # TODO next unless channel pubDate is recent
     foreach my $item ( $feed_obj->get_item() ) {
-        next if $dbh->resultset( 'Item' )->find({ url => $item->link() });
+        my $canonical = $item->link();
+        if ( $item->link() =~ 'feedproxy.google.com' ) {
+            say "Got a feedproxy.google.com url";
+            my $res = $ua->max_redirects( 0 )->get( $item->link() )->res;
+            #say Dumper( $res );
+            say $res->code;
+            my $location = $res->headers->location;
+            my $url = Mojo::URL->new($location);
+            say 'Fixing ' . $item->link();
+            say 'Setting to: ' . $url->scheme . '://' . $url->host . $url->path; 
+            $canonical =  $url->scheme . '://' . $url->host . $url->path;
+            say '$item->url is now: ' . $canonical;
+        }
         next unless $item->title(); # Need titles
         next unless !ref($item->title()) ; # Strings! 
         next unless $item->pubDate();
         my $pubDate = $item->pubDate();
         $pubDate =~ s/^(\d{4}-\d{2}-\d{2}).*$/$1/g;
         my $today = DateTime->now( time_zone => 'floating');
-        my $last_week = $today->subtract( days => 30 );
+        my $last_week = $today->subtract( days => 7 );
         my $epoch = str2time( $pubDate );
         my $feed_date = DateTime->from_epoch({ epoch => $epoch });
         my $date_cmp = DateTime->compare( $feed_date, $last_week );
         next unless $date_cmp == 1;
         $status = 1;
-        my $ua = Mojo::UserAgent->new;
-        my $html
-            = $ua->max_redirects( 5 )->get( $item->link() => { DNT => 1 } )
-            ->res->body;
-        my $obj = HTML::Content::Extractor->new();
-        $obj->analyze( $html );
-        my $main_images = $obj->get_main_images( 1 );
-
-        my $main_text = $obj->get_main_text( 1 );
-        my $image     = '';
-        if ( $main_images ) {
-            my $img = '';
-            $img = $main_images->[0]->{'prop'}->{'src'};
-            if ( $img && $img =~ m!^http://! ) {
-                $image = $img;
-            }
-        }
+        next if $dbh->resultset( 'Item' )->find({ url => $canonical });
+        my $json = $ua->max_redirects( 5 )->get( $ALCHEMY_API . "apikey=$config->{'alchemy_api_key'}" . $ALCHEMY_QUERY . $canonical => { DNT => 1 } )->res->json;
+        say Dumper( $json );
         my $doc = {
-            url     => $item->link() || '',
-            title   => $item->title() || '',
+            url     => $canonical || '',
+            title   => decode_entities( $item->title() ) || '',
             pubdate => $item->pubDate() || '',
             description => summarize(
-                $main_text,
+                decode_entities( $json->{'text'} ),
                 #filter    => 'html',
                 maxlength => 100
             ) || '',
-            content => $main_text || '',
-            image   => $image || '',
+            content => $json->{'text'} || '',
+            image   => $json->{'image'} || '',
             author  => $item->author() || '',
             source_id => $source->id() || '',
         };
         say $doc->{'title'} if $mode eq 'development';
+        #say Dumper( $doc ) if $mode eq 'development';
         _store_item( $doc );
         $item_count++;
     }
-        my $dt = DateTime->now();
-        $source->update({ feed_updated => $dt->datetime() });
-        if ( $status ) { # TODO rethink this logic
-            say "Setting status to active";
-            $source->update({ status => 'Feed active' });
-        } else { 
-            $source->update({ status => 'Not active' });
-            say "Setting status to not active";
-        };
-        $source_count++;
+    my $dt = DateTime->now();
+    $source->update({ feed_updated => $dt->datetime() });
+    if ( $status ) { # TODO rethink this logic
+        say "Setting status to active";
+        $source->update({ status => 'active' });
+    } else { 
+        $source->update({ status => 'stale' });
+        say "Setting status to not active";
+    };
+    $source_count++;
 }
 
 say "Updated $source_count sources and adding $item_count items on this run.";
